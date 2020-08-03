342c373cd4dd110ea8a4f3e6a62bd034f65734c8
stcentre <64156635+stcentre@users.noreply.github.com>
2020-08-03 08:06:39 +0530

    Add files via upload

---
+++ Python_Tutorials/python_multithreading.py
@@ -0,0 +1,19 @@
+"""
+Before starting with MultiThreading lets have a understanding of MultiTasking.
+Executing Sevral task simultaneously is known as multitasking.
+Two types of Multitasking we have :
+1.Process Based Multitasking:
+	Executing Sevral task simultaneously where Each task is a seprate independent Process.
+	For Better Understanding Lets discuss a one example.
+	1.I am writting the code in any text editor.
+	2.Listning the song on the same system
+	3.Downloading some content from internert in the same system.
+	So all these process are running in my system simultaneously but these are indpendent to 
+	each other.
+	Such type of multitasking is known as process based multitasking which is suitable for OS level.
+
+2.Thread Based MultiThreading:
+	within the same process multiple programs are required to execute simultaneously.
+	Each Task is an independent part of the same program such type of multitasking is known as 
+	thread based multiptocessing.
+"""
